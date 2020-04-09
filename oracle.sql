507e875cc7ca42433e7c37edef2e4111f2779b2a
songjinhoon <jinhoon0319@gmail.com>
2020-04-09 11:23:59 +0900

    유림이 머지

--- oracle.sql
+++ oracle.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 create table user1(
     userId varchar(30) PRIMARY KEY,
     userPasswd varchar(20) not null,
@@ -37,6 +38,29 @@ FROM user1
 
 
 		
+SELECT substr(useraddress, 0, instr(useraddress, ' ', 1, 4))
+FROM user1
+where substr(useraddress, 0, instr(useraddress, ' ', 1, 2)) =substr(useraddress, 0, instr(useraddress, ' ', 1, 2)) and useraddress not in('서울 강남구 테헤란로 132 8층') 
+
+=======
+select * from USER1;
+
+delete from USER1;
+
+	SELECT count(userid)
+		FROM user1
+		WHERE userid ='aaa' and userpasswd ='aaa'
+		
+		
+		select * from USER1
+		
+SELECT substr(useraddress, 0, instr(useraddress, ' ', 1, 2))
+FROM user1
+
+update USER1 set useraddress='서울 성동구 용답3길 2 2' where userid='aaa'
+update USER1 set useremailcheck=1
+
+
 SELECT substr(useraddress, 0, instr(useraddress, ' ', 1, 4))
 FROM user1
 where substr(useraddress, 0, instr(useraddress, ' ', 1, 2)) =substr(useraddress, 0, instr(useraddress, ' ', 1, 2)) and useraddress not in('서울 강남구 테헤란로 132 8층') 
